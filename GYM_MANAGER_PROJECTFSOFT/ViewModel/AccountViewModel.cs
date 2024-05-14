40cd1fe6b02b7aa4b761bd55f083b5c86728b62a
Nguyễn Tú <tunguyennhuxyz@gmail.com>
2024-05-14 22:27:50 +0700

    init_file

---
+++ GYM_MANAGER_PROJECTFSOFT/ViewModel/AccountViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GYM_MANAGER_PROJECTFSOFT.Models;
+
+namespace GYM_MANAGER_PROJECTFSOFT.ViewModel
+{
+    public class AccountViewModel
+    {
+        GYM_ManagerEntities db = new GYM_ManagerEntities();
+        public TAIKHOAN TaiKhoan
+        {
+            get;
+            set;
+        }
+
+        public TAIKHOAN find(string username)
+        {
+            return db.TAIKHOANs.Single(acc => acc.TaiKhoan1.Equals(username));
+        }
+
+        public TAIKHOAN login(string username, string password)
+        {
+            return db.TAIKHOANs.Where(acc => acc.TaiKhoan1.Equals(username) && acc.MatKhau.Equals(password)).FirstOrDefault();
+        }
+    }
+}
