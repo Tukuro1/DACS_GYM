d77f8b31cdcca08a2f033e2f7def756167d05ae2
Nguyễn Tú <tunguyennhuxyz@gmail.com>
2024-05-30 17:59:25 +0700

    Reapply "Merge branch 'main' of https://github.com/Tukuro1/DACS_GYM"
    This reverts commit 95b142816cd17c350fdcea4123c18bf86d771d14.

--- GYM_MANAGER_PROJECTFSOFT/Models/Gym_Manager.Designer.cs
+++ GYM_MANAGER_PROJECTFSOFT/Models/Gym_Manager.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Downloads\GymManager_Website\GYM_MANAGER_PROJECTFSOFT\GYM_MANAGER_PROJECTFSOFT\Models\Gym_Manager.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Projects\DACS_GYM\GYM_MANAGER_PROJECTFSOFT\Models\Gym_Manager.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
